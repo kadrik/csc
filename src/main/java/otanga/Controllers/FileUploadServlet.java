7456e19b83c8c15be905ae81f1e21192d44e29a2
Polys <georgiou.polys@gmail.com>
2013-05-12 19:03:44 +0100

    Created a new class FileWriter that extends OutputStream to store uploaded files. This is a fix for the file size limitation problem.

--- src/main/java/otanga/Controllers/FileUploadServlet.java
+++ src/main/java/otanga/Controllers/FileUploadServlet.java
@@ -4,98 +4,91 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.*;
-import java.io.*;
-import org.apache.commons.io.*;
-import org.apache.commons.fileupload.*;
-import org.apache.commons.fileupload.disk.*;
-import org.apache.commons.fileupload.servlet.*;
+import java.io.InputStream;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Polys
- * Date: 10/05/13
- * Time: 15:16
- */
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+
 public class FileUploadServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setStatus(200); // Status: OK
+        resp.setContentType("text/plain");
+
+        FileWriter bos = null;
+        String fileKey = null;
+
         try {
-            resp.setStatus(200); // Status: OK
-            resp.setContentType("text/plain");
 
             ServletFileUpload fileUpload = new ServletFileUpload();
             FileItemIterator items = fileUpload.getItemIterator(req);
+
             while (items.hasNext()) {
                 FileItemStream item = items.next();
-
-                // List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(req);
-            //for (FileItem item : items) {
 
                 if (item.isFormField())
                     continue;
 
                 String fileName = FilenameUtils.getName(item.getName());
-                InputStream fileContent = item.openStream(); // item.getInputStream();
-
-                String fileKey = null;
+                InputStream fileContent = item.openStream();
 
                 int fileSize = fileContent.available();
 
                 resp.getWriter().println("FileName: " + fileName);
-                resp.getWriter().println("FileSize: " + fileSize + " bytes");
                 resp.getWriter().println("ContentType: " + item.getContentType());
                 resp.getWriter().println();
 
                 if (fileSize > 0)
                 {
-                    try{
-                        byte[] data = new byte[fileSize];
-                        if (fileContent.read(data) != fileSize)
-                            throw new IOException();
+                    if (bos == null)
+                        bos = new FileWriter(item.getContentType());
 
-                        resp.getWriter().println("Read " +  data.length + " bytes into the buffer.");
-
-                        resp.getWriter().println();
-                        resp.getWriter().println("Trying to store the file...");
-                        fileKey = FileStorage.storeImage(data, item.getContentType(), resp.getWriter());
-                        resp.getWriter().println("FileStorage.storeImage returned: " +  fileKey);
-                    }
-                    finally {
-                        fileContent.close();
+                    byte[] buffer = new byte[2048];
+                    int count;
+                    while ((count = fileContent.read(buffer)) > 0)
+                    {
+                        bos.write(buffer, 0, count);
                     }
                 }
-                else
-                {
-                    resp.getWriter().println("The file is empty!");
-                }
 
-                if (fileKey != null)
-                {
-                    //TODO: Respond appropriately!
-                    // resp.setStatus(200); // Status: OK
-                    // resp.setContentType("text/plain");
-                    // resp.getWriter().println("Uploaded file '" + fileName + "' with key: " + fileKey);
-
-                    resp.getWriter().println();
-                    resp.getWriter().println("Trying to retrieve the file...");
-
-                    //test reading back the file
-                    String result = FileStorage.retrieveImage(fileKey, resp.getWriter());
-                    resp.getWriter().println("FileStorage.retrieveImage returned: " +  result);
-                }
-                else
-                {
-                    resp.getWriter().println("The file key is null!");
-
-                    // resp.setStatus(406); // Status: Not Acceptable
-                    // resp.setStatus(400); // Status: Bad Request
-                }
+                fileContent.close();
             }
         } catch (FileUploadException e) {
             throw new IOException("Cannot parse multipart request.", e);
+        }
+
+        if (bos != null)
+        {
+            fileKey = bos.fileKey();
+            resp.getWriter().println("Stored " + bos.size() + " bytes into file [" +  fileKey + "].");
+            bos.close();
+        }
+
+        if (fileKey != null)
+        {
+            //TODO: Respond appropriately!
+            // resp.setStatus(200); // Status: OK
+            // resp.setContentType("text/plain");
+            // resp.getWriter().println("Uploaded file '" + fileName + "' with key: " + fileKey);
+
+            resp.getWriter().println();
+            resp.getWriter().println("Trying to retrieve the file...");
+
+            //test reading back the file
+            String result = FileStorage.retrieveImage(fileKey, resp.getWriter());
+            resp.getWriter().println("FileStorage.retrieveImage returned: " +  result);
+        }
+        else
+        {
+            resp.getWriter().println("The file key is null!");
+
+            // resp.setStatus(406); // Status: Not Acceptable
+            // resp.setStatus(400); // Status: Bad Request
         }
     }
 }
---
+++ src/main/java/otanga/Controllers/FileWriter.java
@@ -0,0 +1,80 @@
+package otanga.Controllers;
+
+import com.google.appengine.api.files.AppEngineFile;
+import com.google.appengine.api.files.FileService;
+import com.google.appengine.api.files.FileServiceFactory;
+import com.google.appengine.api.files.FileWriteChannel;
+import com.google.appengine.api.files.GSFileOptions;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Polys
+ * Date: 12/05/13
+ * Time: 18:09
+ */
+public final class FileWriter extends OutputStream {
+
+    private final static FileService _fileService = FileServiceFactory.getFileService();
+    private final static String _bucketName = "otangaimages";
+    private final String _fileKey;
+    private final FileWriteChannel _writeChannel;
+    private int _size;
+
+    public FileWriter(String contentType) throws IOException
+    {
+        if (contentType == null || contentType.length() == 0)
+            contentType = "application/octet-stream";
+
+        _fileKey = UUID.randomUUID().toString().replace("-", "").toLowerCase() + "!" + contentType.replace('/','!');
+
+        GSFileOptions.GSFileOptionsBuilder optionsBuilder = new GSFileOptions.GSFileOptionsBuilder()
+                .setBucket(_bucketName)
+                .setKey(_fileKey)
+                .setMimeType(contentType)
+                .setAcl("private");
+
+        try {
+            AppEngineFile writableFile = _fileService.createNewGSFile(optionsBuilder.build());
+            _writeChannel = _fileService.openWriteChannel(writableFile, true);
+        } catch (IOException e) {
+            throw e;
+        }
+
+        _size = 0;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        byte[] buffer = new byte[1];
+        buffer[0] = (byte)b;
+        _size += _writeChannel.write(ByteBuffer.wrap(buffer));
+    }
+
+    @Override
+    public void write(byte b[], int off, int len) throws IOException {
+        _size += _writeChannel.write(ByteBuffer.wrap(b, off, len));
+    }
+
+    @Override
+    public void write(byte b[]) throws IOException {
+        _size += _writeChannel.write(ByteBuffer.wrap(b));
+    }
+
+    @Override
+    public void close() throws IOException {
+        _writeChannel.closeFinally();
+    }
+
+    public int size() {
+        return _size;
+    }
+
+    public String fileKey() {
+        return _fileKey;
+    }
+}
